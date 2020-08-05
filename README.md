d7d91866ccaad9014def1b809899d3b61f74f8d8
EC2 Default User <ec2-user@ip-172-31-12-60.cn-northwest-1.compute.internal>
2020-08-05 05:01:01 +0000

    init

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # sagemaker-ocr-chinese
 使用Sagemaker 实现中文ocr模型的训练，并部署为Web服务。
+
+
+# 使用Sagemaker 进行训练
+
+[sagemaker](./sagemaker)
+
+
+
+# 本地训练
+
+
+[local](./local)
+
+
+# 建立环境
+```
+conda create -n  ocr python=3.6
+source activate ocr
+pip install -r requirements.txt -i https://mirrors.163.com/pypi/simple/  --default-timeout=3000
+
+
+cd local
+sh ocr_main.sh
+
+```
---
+++ local/ocr_main.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+
+
+if [ ! -d "atte/saved_models/TPS-ResNet-BiLSTM-Attn-Seed1111" ]; then
+  mkdir atte/saved_models/TPS-ResNet-BiLSTM-Attn-Seed1111 -p
+fi
+
+if [ ! -f atte/saved_models/TPS-ResNet-BiLSTM-Attn-Seed1111/best_accuracy.pth ]; then
+    echo 'Download  best_accuracy.pth ......'
+    cd atte/saved_models/TPS-ResNet-BiLSTM-Attn-Seed1111/
+    wget https://dikers-data.s3.cn-northwest-1.amazonaws.com.cn/dataset/best_accuracy.pth
+    cd -
+fi
+
+
+
+if [ ! -d 'craft/weights' ]; then
+    mkdir -p craft/weights
+fi
+
+if [ ! -f craft/weights/craft_mlt_25k.pth ]; then
+    echo 'Download  craft_mlt_25k.pth ......'
+    cd craft/weights
+    wget https://dikers-data.s3.cn-northwest-1.amazonaws.com.cn/dataset/craft_mlt_25k.pth
+    cd -
+fi
+
+
+export PYTHONPATH=./
+
+python3 ocr_main.py  \
+-i 'sample_data/images/' \
+-o 'output/' \
+--cuda==False  \
+--batch_size 64 \
+--label_file_list  'sample_data/chars.txt' \
+--Transformation TPS --FeatureExtraction ResNet --SequenceModeling BiLSTM --Prediction Attn \
+--saved_model atte/saved_models/TPS-ResNet-BiLSTM-Attn-Seed1111/best_accuracy.pth \
+--trained_model craft/weights/craft_mlt_25k.pth
