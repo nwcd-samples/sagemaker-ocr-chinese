24491545ad4819060b41c44753ea6093662669f2
zhangliang <dikers@gmail.com>
2020-08-07 05:45:08 +0000

    add

--- local/ocr_main.sh
+++ local/ocr_main.sh
@@ -36,4 +36,5 @@ python3 ocr_main.py  \
 --label_file_list  'sample_data/chars.txt' \
 --Transformation TPS --FeatureExtraction ResNet --SequenceModeling BiLSTM --Prediction Attn \
 --saved_model atte/saved_models/TPS-ResNet-BiLSTM-Attn-Seed1111/best_accuracy.pth \
---trained_model craft/weights/craft_mlt_25k.pth
+--trained_model craft/weights/craft_mlt_25k.pth \
+--generate_train_data_dir 'output/train/'
